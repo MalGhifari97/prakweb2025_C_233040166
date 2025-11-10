1eed9ac0dd9fc19fc9ce4ea29fa7dda126f657ab
Mal_Ghifari97 <145556126+MalGhifari97@users.noreply.github.com>
2025-11-10 14:23:48 +0700

    Delete pertemuan2/latihan directory

--- pertemuan2/latihan/MVC/TemplateMVC/app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("BASEURL", "http://localhost/Prakweb2025_C_233040166/pertemuan2/latihan/MVC/TemplateMVC/public/");
-
-// DATANASE
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'php_mvc');
