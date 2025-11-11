7e872b0d1bddbb8f557217fb3b96d8a295757e3d
Mal_Ghifari97 <145556126+MalGhifari97@users.noreply.github.com>
2025-11-11 19:41:02 +0700

    Update script.js

--- pertemuan2/latihan/MVC/TemplateMVC/public/js/script.js
+++ pertemuan2/latihan/MVC/TemplateMVC/public/js/script.js
@@ -1,38 +1,39 @@
-$(function() {
-    
-  
-    $('.tombolTambahData').on('click', function() {
-        $('#formModalLabel').html('Tambah Data User');
-        $('.modal-footer button[type=submit]').html('Tambah Data');
-        
-        
-        $('.modal-body form').attr('action', BASEURL + '/user/tambah'); 
-        
-        
-        $('#name').val('');
-        $('#email').val('');
-        $('#id').val(''); 
+$(function () {
+  $(".tombolTambahData").on("click", function () {
+    $("#judulModalLabel").html("Tambah Data Mahasiswa");
+    $(".modal-footer button[type=submit]").html("Tambah Data");
+    $("#nama").val("");
+    $("#nim").val("");
+    $("#jurusan").val("");
+    $("#email").val("");
+    $(".modal-body form").attr(
+      "action",
+      "http://localhost/belajar/phpmvc/sesi13/public/mahasiswa/tambah"
+    );
+  });
+  $(".tampilModalUbah").on("click", function () {
+    $("#judulModalLabel").html("Ubah Data Mahasiswa");
+    $(".modal-footer button[type=submit]").html("Ubah Data");
+    $(".modal-body form").attr(
+      "action",
+      "http://localhost/belajar/phpmvc/sesi13/public/mahasiswa/ubah"
+    );
+
+    const id = $(this).data("id");
+
+    // karena objek pake {}
+    $.ajax({
+      url: "http://localhost/belajar/phpmvc/sesi13/public/mahasiswa/getubah",
+      data: { id: id },
+      method: "post",
+      dataType: "json",
+      success: function (data) {
+        $("#nama").val(data.nama);
+        $("#nim").val(data.nim);
+        $("#jurusan").val(data.jurusan);
+        $("#email").val(data.email);
+        $("#id").val(data.id);
+      },
     });
-
-    $('.tombolUbah').on('click', function() {
-        $('#formModalLabel').html('Ubah Data User');
-        $('.modal-footer button[type=submit]').html('Ubah Data');
-        
-        $('.modal-body form').attr('action', BASEURL + '/user/ubah'); 
-
-        const id = $(this).data('id');
-        
-        $.ajax({
-            url: BASEURL + '/user/getubah',
-            data: {id : id},
-            method: 'post',
-            dataType: 'json',
-            success: function(data) {
-                $('#name').val(data.name); 
-                $('#email').val(data.email);
-                $('#id').val(data.id);
-            }
-        });
-    });
-
+  });
 });
